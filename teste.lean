03a1fc362a649a729f7f6dedd4da782d0c5aa41e
Vitugome3s <victor.aguiar08@hotmail.com>
2023-09-09 11:14:28 -0300

    Function World 1/9

---
+++ teste.lean
@@ -0,0 +1,6 @@
+Suponha ¬(∀x)[φ(x)]. -- ¬(∀x)[φ(x)] ⊢ (∃x)[¬φ(x)]
+Escolho x. -- ¬(∀x)[φ(x)] ⊢ ¬φ(x)
+Suponha φ(x). -- ¬(∀x)[φ(x)], φ(x) ⊢ ⊥
+Aplique ¬(∀x)[φ(x)] no φ(x) para inferir ⊥. -- ¬(∀x)[φ(x)], φ(x), ⊥ ⊢ ⊥
+Contradição.
+∎
